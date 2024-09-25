446d8ba36c7858cf1b4b448c33d6248d85768f94
kerllon07 <129960911+kerllon07@users.noreply.github.com>
2024-09-25 08:44:20 -0300

    Add files via upload

---
+++ Aula08_Projeto_Variaveis/script.js
@@ -0,0 +1,35 @@
+//Criando uma variavel var
+
+var nome = "Adamastor"
+console.log(nome)
+
+if(nome == "Adamastor"){
+
+    var escola = "CESF";
+    console.log(escola);
+}
+
+console.log(escola);
+escola = "Sagrada";
+console.log(escola);
+
+//Let
+
+if(nome == "Adamastor"){
+    let curso = "DS";
+    console.log(curso);
+}
+
+curso = "culinaria"
+
+console.log(curso);
+
+//Const
+
+const cargo = "Estudante";
+console.log(cargo);
+
+//Retribuir
+
+
+
